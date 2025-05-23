ee8011e344871705d3920944a5826920b9817c13
rachit-bhatt <Ratchtbhatt2000@gmail.com>
2025-05-22 20:26:43 -0400

    Merged Changed from VS.

--- Pharmacy-Management-System/PMS/PMS/obj/Debug/net8.0-windows/PMS.AssemblyInfo.cs
+++ Pharmacy-Management-System/PMS/PMS/obj/Debug/net8.0-windows/PMS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bac2af3d72bf733a1cbf66933ee4edd47c0a88fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de69fc2599b1c9efdce711f7f13efa8e04578996")]
 [assembly: System.Reflection.AssemblyProductAttribute("PMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
