615188053f7aac1247eb4569f19bdd1bb2c1f0e9
rachit-bhatt <Ratchtbhatt2000@gmail.com>
2025-06-05 18:15:42 -0400

    Implemented Converters for UI to enable/disable UI elements and displaying View/Edit icon in the window.

--- Pharmacy-Management-System/PMS/PMS/ViewModels/PatientDataViewModel.cs
+++ Pharmacy-Management-System/PMS/PMS/ViewModels/PatientDataViewModel.cs
@@ -5,11 +5,18 @@ namespace PMS.ViewModels
     public class PatientDataViewModel : ViewModelBase
     {
         private PatientData _patient = new();
+        private bool _isEditing;
 
         public PatientData Patient
         {
             get => _patient;
             set => SetProperty(ref _patient, value);
+        }
+
+        public bool IsEditing
+        {
+            get => _isEditing;
+            set => SetProperty(ref _isEditing, value);
         }
 
         public PatientDataViewModel()
