5306d68485bf053fe42c932889231d1b0dc67da0
rachit-bhatt <Ratchtbhatt2000@gmail.com>
2025-07-03 21:51:00 -0400

    Validators added - testing going on.

---
+++ Pharmacy-Management-System/PMS/PMS/Views/Controls/ValidatedField.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace PMS.Views.Controls
+{
+    public enum ErrorPosition
+    {
+        Bottom,
+        Top,
+        Left,
+        Right
+    }
+
+    public partial class ValidatedField : UserControl
+    {
+        public static readonly DependencyProperty ErrorProperty =
+            DependencyProperty.Register(nameof(Error), typeof(string), typeof(ValidatedField), new PropertyMetadata(string.Empty));
+
+        public static readonly DependencyProperty ErrorPositionProperty =
+            DependencyProperty.Register(nameof(ErrorPosition), typeof(ErrorPosition), typeof(ValidatedField), new PropertyMetadata(ErrorPosition.Bottom));
+
+        public static readonly DependencyProperty FieldContentProperty =
+            DependencyProperty.Register(nameof(FieldContent), typeof(object), typeof(ValidatedField), new PropertyMetadata(null));
+
+        public string Error
+        {
+            get => (string)GetValue(ErrorProperty);
+            set => SetValue(ErrorProperty, value);
+        }
+
+        public ErrorPosition ErrorPosition
+        {
+            get => (ErrorPosition)GetValue(ErrorPositionProperty);
+            set => SetValue(ErrorPositionProperty, value);
+        }
+
+        public object FieldContent
+        {
+            get => GetValue(FieldContentProperty);
+            set => SetValue(FieldContentProperty, value);
+        }
+
+        public ValidatedField()
+        {
+            InitializeComponent();
+        }
+    }
+}
