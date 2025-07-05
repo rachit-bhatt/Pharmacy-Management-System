2a5e3f2621eb7b72d459f3f1695d2523cfae3adc
rachit-bhatt <Ratchtbhatt2000@gmail.com>
2025-07-04 21:56:37 -0400

    ✅ Commanized Templates/Themes Codes. ✅ Resolved Binding Errors.

--- Pharmacy-Management-System/PMS/PMS/obj/Debug/net8.0-windows/App.g.i.cs
+++ Pharmacy-Management-System/PMS/PMS/obj/Debug/net8.0-windows/App.g.i.cs
@@ -59,7 +59,7 @@ namespace PMS {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/PMS;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/PMS;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
