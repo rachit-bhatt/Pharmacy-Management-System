792a9ad31f7f2ddebd0a19a04b4087c3ddbf7cbb
rachit-bhatt <Ratchtbhatt2000@gmail.com>
2025-09-16 18:14:47 -0400

    Testing successful.

--- Pharmacy-Management-System/PMS/Tests/obj/Debug/net8.0-windows/Tests.AssemblyInfo.cs
+++ Pharmacy-Management-System/PMS/Tests/obj/Debug/net8.0-windows/Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd6ea6cb84f88bbe94979cc40406be76756bfe00")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+672f67eb29130f4733ddc644c4f90fbf7b67fe8f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
